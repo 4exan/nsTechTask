2367fadeb323dceba079f0415f812d36578651c9
4exan <ivan.chekhanovskyi@gmail.com>
2025-06-24 16:12:38 +0300

    init commit

---
+++ src/test/java/ua/dev/techtask/TechTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.dev.techtask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
