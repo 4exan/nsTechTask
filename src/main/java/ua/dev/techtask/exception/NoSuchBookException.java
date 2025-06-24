1365d2b36e12eeb3ec90774a115f59f57c8febf8
4exan <ivan.chekhanovskyi@gmail.com>
2025-06-24 19:05:39 +0300

    feat: added exceptions

---
+++ src/main/java/ua/dev/techtask/exception/NoSuchBookException.java
@@ -0,0 +1,5 @@
+package ua.dev.techtask.exception;
+
+public class NoSuchBookException extends RuntimeException{
+    
+}
---
+++ src/main/java/ua/dev/techtask/exception/NoSuchMemberException.java
@@ -0,0 +1,5 @@
+package ua.dev.techtask.exception;
+
+public class NoSuchMemberException extends RuntimeException{
+    
+}
