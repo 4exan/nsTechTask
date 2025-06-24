c0e8c219cf3745f057a29b6f89aace1f71da22f0
4exan <ivan.chekhanovskyi@gmail.com>
2025-06-24 21:00:20 +0300

    feat: added Borrow entity

---
+++ src/main/java/ua/dev/techtask/entity/Borrow.java
@@ -0,0 +1,68 @@
+package ua.dev.techtask.entity;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Table(name = "borrows")
+@Entity
+public class Borrow {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private long id;
+  @ManyToOne
+  @JoinColumn(name = "member_id")
+  private Member member;
+  @ManyToOne
+  @JoinColumn(name = "book_id")
+  private Book book;
+  private LocalDateTime borrowDate;
+  private LocalDateTime returnDate;
+
+  public long getId() {
+    return this.id;
+  }
+
+  public void setId(long id) {
+    this.id = id;
+  }
+
+  public Member getMember() {
+    return this.member;
+  }
+
+  public void setMember(Member member) {
+    this.member = member;
+  }
+
+  public Book getBook() {
+    return this.book;
+  }
+
+  public void setBook(Book book) {
+    this.book = book;
+  }
+
+  public LocalDateTime getBorrowDate() {
+    return this.borrowDate;
+  }
+
+  public void setBorrowDate(LocalDateTime borrowDate) {
+    this.borrowDate = borrowDate;
+  }
+
+  public LocalDateTime getReturnDate() {
+    return this.returnDate;
+  }
+
+  public void setReturnDate(LocalDateTime returnDate) {
+    this.returnDate = returnDate;
+  }
+}
