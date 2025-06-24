d18e8b0e099490fe70c10983413f29a8c6c7239b
4exan <ivan.chekhanovskyi@gmail.com>
2025-06-24 18:53:53 +0300

    fix: fixed wrong 'Member' import

--- src/main/java/ua/dev/techtask/repository/MemberRepository.java
+++ src/main/java/ua/dev/techtask/repository/MemberRepository.java
@@ -1,9 +1,9 @@
 package ua.dev.techtask.repository;
 
-import java.lang.reflect.Member;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import ua.dev.techtask.entity.Member;
 
 @Repository
 public interface MemberRepository extends JpaRepository<Member, Long>{
