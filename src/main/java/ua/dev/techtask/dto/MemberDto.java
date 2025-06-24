6d9ffa109d55aa0266a6a96d27927247a93d9f5f
4exan <ivan.chekhanovskyi@gmail.com>
2025-06-24 18:58:06 +0300

    feat: added Member DTO

---
+++ src/main/java/ua/dev/techtask/dto/MemberDto.java
@@ -0,0 +1,61 @@
+package ua.dev.techtask.dto;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+import ua.dev.techtask.entity.Member;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class MemberDto {
+    
+private long id;
+private String name;
+private LocalDateTime membershipDate;
+
+private List<Member> memberList;
+private Member member;
+
+   public long getId(){
+        return this.id;
+    }
+
+    public void setId(long id){
+        this.id = id;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public LocalDateTime getMembershipDate(){
+        return this.membershipDate;
+    }
+
+    public void setMembershipDate(LocalDateTime membershipDate){
+        this.membershipDate = membershipDate;
+    }
+
+    public List<Member> getMemberList(){
+        return this.memberList;
+    }
+
+    public void setMemberList(List<Member> memberList){
+        this.memberList = memberList;
+    }
+
+    public Member getMember(){
+        return this.member;
+    }
+
+    public void setMember(Member member){
+        this.member = member;
+    }
+}
