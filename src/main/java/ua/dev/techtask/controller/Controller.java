b7f7830d4a27e38e45efb4596b0232f2ebe0cfdd
4exan <ivan.chekhanovskyi@gmail.com>
2025-06-24 20:03:27 +0300

    feat: added member CRUD endpoints

--- src/main/java/ua/dev/techtask/controller/Controller.java
+++ src/main/java/ua/dev/techtask/controller/Controller.java
@@ -13,34 +13,59 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import ua.dev.techtask.dto.BookDto;
+import ua.dev.techtask.dto.MemberDto;
 import ua.dev.techtask.entity.Book;
+import ua.dev.techtask.entity.Member;
 import ua.dev.techtask.service.BookService;
+import ua.dev.techtask.service.MemberService;
 
 @RestController
 @RequestMapping("/api")
 public class Controller {
 
-    @Autowired
-    private BookService bookService;
+  @Autowired
+  private BookService bookService;
+  @Autowired
+  private MemberService memberService;
 
-    @GetMapping("/books/get-all")
-    public ResponseEntity<List<Book>> getAllBooks(){
-        return ResponseEntity.ok(bookService.getAllBooks());
-    }
+  @GetMapping("/books")
+  public ResponseEntity<List<Book>> getAllBooks() {
+    return ResponseEntity.ok(bookService.getAllBooks());
+  }
 
-    @PostMapping("/books")
-    public void createNewBook(@RequestBody BookDto req){
-        bookService.createBook(req);
-    }
+  @PostMapping("/books")
+  public void createNewBook(@RequestBody BookDto req) {
+    bookService.createBook(req);
+  }
 
-    @PostMapping("/books/edit")
-    public void editBook(@RequestBody BookDto req){
-        bookService.editBook(req);
-    }
+  @PostMapping("/books/edit")
+  public void editBook(@RequestBody BookDto req) {
+    bookService.editBook(req);
+  }
 
-    @DeleteMapping("/books/{id}")
-    public void deleteBook(@PathVariable Long id){
-        bookService.deleteBook(id);
-    }
-    
+  @DeleteMapping("/books/{id}")
+  public void deleteBook(@PathVariable Long id) {
+    bookService.deleteBook(id);
+  }
+
+  @GetMapping("/members")
+  public ResponseEntity<List<Member>> getAllMembers() {
+    return ResponseEntity.ok(memberService.getAllMembers());
+  }
+
+  @PostMapping("/members")
+  public void createNewMember(@RequestBody MemberDto req) {
+    memberService.createNewMember(req);
+  }
+
+  @PostMapping("/members/edit")
+  public void editMember(@RequestBody MemberDto req) {
+    memberService.editMember(req);
+  }
+
+  @DeleteMapping("/members/{id}")
+  public void deleteMember(@PathVariable Long id) {
+    memberService.deleteMember(id);
+  }
+
 }
