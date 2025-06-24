f3be35725616ba6948925f0d7d91050d62c042fb
4exan <ivan.chekhanovskyi@gmail.com>
2025-06-24 18:48:12 +0300

    feat: added Member Repository

---
+++ src/main/java/ua/dev/techtask/repository/MemberRepository.java
@@ -0,0 +1,11 @@
+package ua.dev.techtask.repository;
+
+import java.lang.reflect.Member;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MemberRepository extends JpaRepository<Member, Long>{
+    
+}
