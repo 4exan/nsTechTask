c5a6139afe3b1d4eb8512d68e8d125856867bb05
4exan <ivan.chekhanovskyi@gmail.com>
2025-06-24 20:15:22 +0300

    feat: added Member Service

--- src/main/java/ua/dev/techtask/service/MemberService.java
+++ src/main/java/ua/dev/techtask/service/MemberService.java
@@ -1,6 +1,7 @@
 package ua.dev.techtask.service;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,39 +13,43 @@ import ua.dev.techtask.repository.MemberRepository;
 
 @Service
 public class MemberService {
-    
-    @Autowired
-    private MemberRepository memberRepository;
 
-    public Member getMemberById(long id){
-        Member member = memberRepository.findById(id).orElseThrow(() -> new NoSuchMemberException());
-        return member;
-    }
+  @Autowired
+  private MemberRepository memberRepository;
 
-    public void createNewMember(MemberDto req){
-        Member member = new Member();
-        member.setName(req.getName());
-        member.setMembershipDate(LocalDateTime.now());
-        saveMemberToDB(member);
-    }
+  public List<Member> getAllMembers() {
+    return memberRepository.findAll();
+  }
 
-    public void editMember(MemberDto req){
-        Member member = memberRepository.findById(req.getId()).orElseThrow(() -> new RuntimeException("No such member"));
-        member.setName(req.getName());
-        saveMemberToDB(member);
-    }
+  public Member getMemberById(long id) {
+    Member member = memberRepository.findById(id).orElseThrow(() -> new NoSuchMemberException());
+    return member;
+  }
 
-    //TODO impl deletion logic
-    public void deleteMember(long id){
-        memberRepository.deleteById(id);
-    }
+  public void createNewMember(MemberDto req) {
+    Member member = new Member();
+    member.setName(req.getName());
+    member.setMembershipDate(LocalDateTime.now());
+    saveMemberToDB(member);
+  }
 
-    private void saveMemberToDB(Member member){
-        try{
-            memberRepository.save(member);
-        }catch(Exception e){
-            throw new RuntimeException("Error occured when saving member to DB -> ", e);
-        }
+  public void editMember(MemberDto req) {
+    Member member = memberRepository.findById(req.getId()).orElseThrow(() -> new NoSuchMemberException());
+    member.setName(req.getName());
+    saveMemberToDB(member);
+  }
+
+  // TODO impl deletion logic
+  public void deleteMember(long id) {
+    memberRepository.deleteById(id);
+  }
+
+  private void saveMemberToDB(Member member) {
+    try {
+      memberRepository.save(member);
+    } catch (Exception e) {
+      throw new RuntimeException("Error occured when saving member to DB -> ", e);
     }
+  }
 
 }
