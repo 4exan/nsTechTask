ac08a6cf01b5498cab75c6fc0772b61d289953ca
4exan <ivan.chekhanovskyi@gmail.com>
2025-06-24 21:45:05 +0300

    feat: added borrow repo with counters

--- src/main/java/ua/dev/techtask/repository/BorrowRepository.java
+++ src/main/java/ua/dev/techtask/repository/BorrowRepository.java
@@ -1,5 +1,7 @@
 package ua.dev.techtask.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,9 @@ import ua.dev.techtask.entity.Borrow;
 
 @Repository
 public interface BorrowRepository extends JpaRepository<Borrow, Long> {
+  Optional<Borrow> findByMemberIdAndBookIdAndReturnDateIsNull(Long memberId, Long bookId);
 
+  int countByMemberIdAndReturnDateIsNull(Long memberId);
+
+  int countByBookIdAndReturnDateIsNull(Long bookId);
 }
