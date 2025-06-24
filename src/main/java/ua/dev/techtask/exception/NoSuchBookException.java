aff6008916c598ac70ed874ff2d90631131121a1
4exan <ivan.chekhanovskyi@gmail.com>
2025-06-24 19:10:57 +0300

    fix: added constructors to exceptions

--- src/main/java/ua/dev/techtask/exception/NoSuchBookException.java
+++ src/main/java/ua/dev/techtask/exception/NoSuchBookException.java
@@ -2,4 +2,14 @@ package ua.dev.techtask.exception;
 
 public class NoSuchBookException extends RuntimeException{
     
+    public NoSuchBookException(){}
+
+    public NoSuchBookException(String errorMessage){
+        super(errorMessage);
+    }
+
+    public NoSuchBookException(String errorMessage, Throwable err){
+        super(errorMessage, err);
+    }
+
 }
--- src/main/java/ua/dev/techtask/exception/NoSuchMemberException.java
+++ src/main/java/ua/dev/techtask/exception/NoSuchMemberException.java
@@ -1,5 +1,14 @@
 package ua.dev.techtask.exception;
 
 public class NoSuchMemberException extends RuntimeException{
-    
+
+    public NoSuchMemberException(){}
+
+    public NoSuchMemberException(String errorMessage){
+        super(errorMessage);
+    }
+
+    public NoSuchMemberException(String errorMessage, Throwable err){
+        super(errorMessage, err);
+    }
 }
