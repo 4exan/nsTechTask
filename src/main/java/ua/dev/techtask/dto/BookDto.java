0a91e9b8759a95c295e4409854f5281510492104
4exan <ivan.chekhanovskyi@gmail.com>
2025-06-25 15:04:22 +0300

    ref: added constructor

--- src/main/java/ua/dev/techtask/dto/BookDto.java
+++ src/main/java/ua/dev/techtask/dto/BookDto.java
@@ -10,60 +10,66 @@ import ua.dev.techtask.entity.Book;
 @JsonIgnoreProperties(ignoreUnknown = true)
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class BookDto {
-    
-    private long id;
-    private String title;
-    private String author;
-    private int amount;
 
-    private List<Book> bookList;
-    private Book book;
+  private long id;
+  private String title;
+  private String author;
+  private int amount;
 
- public long getId(){
-    return this.id;
-  }
+  private List<Book> bookList;
+  private Book book;
 
-  public void setId(long id){
-    this.id = id;
-  }
-
-  public String getTitle(){
-    return this.title;
-  }
-
-  public void setTitle(String title){
+  public BookDto(String title, String author, int amount) {
     this.title = title;
-  }
-
-  public String getAuthor(){
-    return this.author;
-  }
-
-  public void setAuthor(String author){
     this.author = author;
-  }
-
-  public int getAmount(){
-    return this.amount;
-  }
-
-  public void setAmount(int amount){
     this.amount = amount;
   }
 
-  public List<Book> getBookList(){
+  public long getId() {
+    return this.id;
+  }
+
+  public void setId(long id) {
+    this.id = id;
+  }
+
+  public String getTitle() {
+    return this.title;
+  }
+
+  public void setTitle(String title) {
+    this.title = title;
+  }
+
+  public String getAuthor() {
+    return this.author;
+  }
+
+  public void setAuthor(String author) {
+    this.author = author;
+  }
+
+  public int getAmount() {
+    return this.amount;
+  }
+
+  public void setAmount(int amount) {
+    this.amount = amount;
+  }
+
+  public List<Book> getBookList() {
     return this.bookList;
   }
 
-  public void setBookList(List<Book> bookList){
+  public void setBookList(List<Book> bookList) {
     this.bookList = bookList;
   }
 
-  public Book getBook(){
+  public Book getBook() {
     return this.book;
   }
 
-  public void setBook(Book book){
+  public void setBook(Book book) {
     this.book = book;
   }
 
