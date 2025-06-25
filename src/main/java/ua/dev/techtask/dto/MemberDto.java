e8f3b1324fb5a34788c25119dff3da5cfbde309f
4exan <ivan.chekhanovskyi@gmail.com>
2025-06-25 18:14:38 +0300

    ref: added constructor

--- src/main/java/ua/dev/techtask/dto/MemberDto.java
+++ src/main/java/ua/dev/techtask/dto/MemberDto.java
@@ -1,12 +1,9 @@
 package ua.dev.techtask.dto;
 
 import java.time.LocalDateTime;
-import java.util.List;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
-
-import ua.dev.techtask.entity.Member;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 @JsonInclude(JsonInclude.Include.NON_NULL)
@@ -15,6 +12,10 @@ public class MemberDto {
   private long id;
   private String name;
   private LocalDateTime membershipDate;
+
+  public MemberDto(String name) {
+    this.name = name;
+  }
 
   public long getId() {
     return this.id;
