8791d22a17fe6481817d5e6d2ceb09f1ba7d9d21
4exan <ivan.chekhanovskyi@gmail.com>
2025-06-25 15:59:34 +0300

    feat: added borrow stats endpoints

--- src/main/java/ua/dev/techtask/controller/LibraryController.java
+++ src/main/java/ua/dev/techtask/controller/LibraryController.java
@@ -1,12 +1,19 @@
 package ua.dev.techtask.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import ua.dev.techtask.dto.BorrowDto;
+import ua.dev.techtask.dto.BorrowedBookStat;
+import ua.dev.techtask.entity.Book;
 import ua.dev.techtask.service.LibraryService;
 
 @RestController
@@ -24,5 +31,20 @@ public class LibraryController {
   @PostMapping("/return")
   public void returnBook(@RequestBody BorrowDto req) {
     libraryService.returnBook(req);
+  }
+
+  @GetMapping("/borrowed/{memberName}")
+  public ResponseEntity<List<Book>> getAllBorrowedBooksByMemberName(@PathVariable String memberName) {
+    return ResponseEntity.ok(libraryService.getAllBorrowedBooksByMemberName(memberName));
+  }
+
+  @GetMapping("/borrowed/disctinct")
+  public ResponseEntity<List<String>> getAllBorrowedBooks() {
+    return ResponseEntity.ok(libraryService.findDistinctBorrowedBookTitles());
+  }
+
+  @GetMapping("/borrowed/statistics")
+  public ResponseEntity<List<BorrowedBookStat>> getBorrowedBookStatistics() {
+    return ResponseEntity.ok(libraryService.findBorrowedBookStats());
   }
 }
